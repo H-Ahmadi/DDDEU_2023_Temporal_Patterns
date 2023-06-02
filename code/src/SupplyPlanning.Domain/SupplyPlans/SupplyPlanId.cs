0a640cda1a9d647940a0c07b6da754f5f91c2823
Hadi Ahmadi <ahmadi.hadi@outlook.com>
2023-06-02 13:03:45 +0330

    adding an identifier to supply plan

---
+++ code/src/SupplyPlanning.Domain/SupplyPlans/SupplyPlanId.cs
@@ -0,0 +1,17 @@
+﻿using SupplyPlanning.Domain.Core;
+
+namespace SupplyPlanning.Domain.SupplyPlans;
+
+public class SupplyPlanId : ValueObject
+{
+    public Guid Value { get; private set; }
+    public SupplyPlanId(Guid value)
+    {
+        Value = value;
+    }
+    public static SupplyPlanId New() => new SupplyPlanId(Guid.NewGuid());
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
