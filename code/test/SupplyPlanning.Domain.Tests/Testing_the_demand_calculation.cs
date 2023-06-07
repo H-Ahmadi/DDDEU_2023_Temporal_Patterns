f5b8ef07a1df2078d68432133c8b4fab53f04188
Hadi Ahmadi <ahmadi.hadi@outlook.com>
2023-06-07 16:20:07 +0200

    changing tests

--- code/test/SupplyPlanning.Domain.Tests/Testing_the_demand_calculation.cs
+++ code/test/SupplyPlanning.Domain.Tests/Testing_the_demand_calculation.cs
@@ -42,7 +42,8 @@ public class Testing_the_demand_calculation : TimeTravelingTest
         Clock.TravelTo("2023-01-01");
         var demands = new List<DemandItem>() {
             new(50, "KG", Apple), 
-            new(30, "KG", Banana)
+            new(30, "KG", Banana),
+            new(20, "KG", Orange),
         };
         var plan = Schedule.SupplyPlan()
                             .UsingTheClock(Clock)
