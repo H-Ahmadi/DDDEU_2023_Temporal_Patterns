e6644f5e578ef978472b798b70c2db5295a14de8
Hadi Ahmadi <ahmadi.hadi@outlook.com>
2023-06-07 12:51:47 +0200

    changing the sample data in tests

--- code/test/SupplyPlanning.Domain.Tests/Testing_history_of_address_for_customer.cs
+++ code/test/SupplyPlanning.Domain.Tests/Testing_history_of_address_for_customer.cs
@@ -9,43 +9,43 @@ namespace SupplyPlanning.Domain.Tests
         [Fact]
         public void introducing_a_customer_with_an_address()
         {
-            var customer = new Customer(1, "Hadi", "Ahmadi", "Tehran, Iran");
+            var customer = new Customer(1, "Olivia", "Johnson", "123 Maple Street");
 
-            customer.Address.GetValue().Should().Be("Tehran, Iran");
+            customer.Address.GetValue().Should().Be("123 Maple Street");
         }
 
         [Fact]
-        public void pointing_to_an_address_with_a_value_of_temporal_property()
+        public void pointing_to_an_address_without_specifying_time_returns_the_last_address()
         {
-            var customer = new Customer(1, "Hadi", "Ahmadi", "Tehran, Iran");
+            var customer = new Customer(1, "Olivia", "Johnson", "123 Maple Street");
 
             string? address = customer.Address;
-            address.Should().Be("Tehran, Iran");
+            address.Should().Be("123 Maple Street");
         }
 
         [Fact]
         public void getting_the_current_address_after_changing_the_address()
         {
             Clock.TravelThroughTime("2023-01-01");
-            var customer = new Customer(1, "Hadi", "Ahmadi", "Tehran, Iran", Clock);
+            var customer = new Customer(1, "Olivia", "Johnson", "123 Maple Street", Clock);
 
             Clock.TravelThroughTime("2023-03-01");
-            customer.ChangeAddress("Karaj, Iran", Clock);
+            customer.ChangeAddress("456 Oak Avenue", Clock);
 
             var actualAddress = customer.Address.GetValue();
-            actualAddress.Should().Be("Karaj, Iran");
+            actualAddress.Should().Be("456 Oak Avenue");
         }
 
         [Theory]
-        [InlineData("2023-02-01", "Tehran, Iran")]
-        [InlineData("2023-04-01", "Karaj, Iran")]
+        [InlineData("2023-02-01", "123 Maple Street")]
+        [InlineData("2023-04-01", "456 Oak Avenue")]
         public void getting_an_address_in_a_specific_time(string date, string expectedAddress)
         {
             Clock.TravelThroughTime("2023-01-01");
-            var customer = new Customer(1, "Hadi", "Ahmadi", "Tehran, Iran", Clock);
+            var customer = new Customer(1, "Olivia", "Johnson", "123 Maple Street", Clock);
 
             Clock.TravelThroughTime("2023-03-01");
-            customer.ChangeAddress("Karaj, Iran", Clock);
+            customer.ChangeAddress("456 Oak Avenue", Clock);
 
             var dateTime = DateTime.Parse(date);
             var actualAddress = customer.Address.EffectiveValueAt(dateTime);
